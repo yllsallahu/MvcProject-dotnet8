3a9483d65b15fc22832df163c713a327072b4ad1
yllsallahu <ys68687@ubt-uni.net>
2025-01-20 23:05:14 +0100

    Add project files.

---
+++ MvcProject-dotnet8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcProject_dotnet8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
